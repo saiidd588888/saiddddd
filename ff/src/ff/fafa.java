205c93413b3f195cc51d1ddd634e5b5e9c185953
1_web20_09 <1_web20_09@A08_PC04.JOVELLANOS.EDU>
2020-01-29 16:10:39 +0100

    gsdrgsr

--- ff/src/ff/fafa.java
+++ ff/src/ff/fafa.java
@@ -4,5 +4,6 @@ public class fafa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("icgdfts");
+        System.out.println("zfsdfs");
 	}
 }
