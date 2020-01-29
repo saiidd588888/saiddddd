58dc3134b7eea8e93dd5b2206c2d9b52f03c36d6
1_web20_09 <1_web20_09@A08_PC04.JOVELLANOS.EDU>
2020-01-29 15:32:37 +0100

    saxa

---
+++ ff/src/ff/fafa.java
@@ -0,0 +1,8 @@
+package ff;
+
+public class fafa {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("icgdfts");
+	}
+}
---
+++ ff/src/ff/hola.java
@@ -0,0 +1,10 @@
+package ff;
+
+public class hola {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("said");
+	}
+
+
+}
